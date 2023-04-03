10eff38432e22e72753cad5e422b0c704102a380
CorbensAGit <corb25@hotmail.co.uk>
2023-04-03 01:50:20 +0100

    UI tweak and small level1 change
    - fixed double square highlight when coming from settings --> main
    
    - added two starting barrels to level 1
    
    - added vignette to main menu for 1920s feeling

--- Minimum Wage/Assets/Scripts/UI/SC_MainMenu.cs
+++ Minimum Wage/Assets/Scripts/UI/SC_MainMenu.cs
@@ -6,11 +6,15 @@ public class SC_MainMenu : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject CreditsMenu;
+    public SpriteRenderer SettingsSquare;
+    public SpriteRenderer BackSquare;
+    public Color transparent;
 
     // Start is called before the first frame update
     void Start()
     {
         MainMenuButton();
+        transparent = new Color(0, 0, 0, 0);
     }
 
     public void PlayNowButton()
@@ -19,18 +23,22 @@ public class SC_MainMenu : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level01");
     }
 
-    public void CreditsButton()
+    public void SettingsButton()
     {
         // Show Credits Menu
         MainMenu.SetActive(false);
+        // reset the back square
         CreditsMenu.SetActive(true);
+        BackSquare.color = transparent;
     }
 
     public void MainMenuButton()
     {
         // Show Main Menu
-        MainMenu.SetActive(true);
         CreditsMenu.SetActive(false);
+        // reset the settings square
+        MainMenu.SetActive(true);
+        SettingsSquare.color = transparent;
     }
 
     public void QuitButton()
