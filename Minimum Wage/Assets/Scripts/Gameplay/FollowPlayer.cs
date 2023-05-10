f6d7cad35e35f63e432a596e0ad34ca25438ea22
CorbensAGit <corb25@hotmail.co.uk>
2023-05-10 18:08:11 +0100

    safety save

--- Minimum Wage/Assets/Scripts/Gameplay/FollowPlayer.cs
+++ Minimum Wage/Assets/Scripts/Gameplay/FollowPlayer.cs
@@ -4,6 +4,7 @@ public class FollowPlayer : MonoBehaviour {
 
     public Transform Player;
     public Transform TopPlatform;
+    public Transform BottomPlatform;
     public Transform LWall;
     public Transform RWall;
     private float middlePoint;
@@ -17,7 +18,7 @@ public class FollowPlayer : MonoBehaviour {
     void Update () {
         if (Player != null)
         {
-            if (Player.transform.position.y < (TopPlatform.transform.position.y+2) && Player.transform.position.y > 0)
+            if (Player.transform.position.y < (TopPlatform.transform.position.y+2) && Player.transform.position.y > BottomPlatform.transform.position.y+12)
             {
 
                 transform.position = new Vector3(middlePoint, Player.transform.position.y, -28);
--- Minimum Wage/Assets/Scripts/Managers/AchievementNotificationManager.cs
+++ Minimum Wage/Assets/Scripts/Managers/AchievementNotificationManager.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
+[RequireComponent(typeof(Animator))]
 public class AchievementNotificationManager : MonoBehaviour
 {
-    //[SerializeField] Text achievementTitleLablel;
+    [SerializeField] TMP_Text achievementTitleLablel;
+
+    private Animator curr_animator;
     // Start is called before the first frame update
     void Awake()
     {
-        
+        curr_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     public void ShowNotification(Achievement achievement)
     {
         Debug.Log("earned: " + achievement.id);
+        achievementTitleLablel.text = achievement.title;
+        curr_animator.SetTrigger("Appear");
     }
 }
